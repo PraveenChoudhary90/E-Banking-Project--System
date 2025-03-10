dd52cc3095d3c530b426dfc8cfe392ae800adc21
praveen choudhary <pc852180@gmail.com>
2025-03-10 23:34:19 +0530

    e-banking-project

---
+++ Server/app.js
@@ -0,0 +1,25 @@
+const express  =require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors"); 
+require("dotenv").config();
+
+
+app.use(cors());
+// Parse incoming requests with JSON payloads
+app.use(bodyParser.json());
+
+// Parse incoming requests with urlencoded payloads
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.STRING).then(()=>{
+    console.log("DB IS CONNECTED");
+})
+
+
+
+const port = process.env.PORT;
+app.listen(port,()=>{
+    console.log(`Server is running on ${port} port`);
+})
---
+++ Server/model/customerModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const customerSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        require: true
+    },
+     lastname: {
+        type: String,
+        require: true
+    },
+    email: { type: String,
+         required: true,
+          unique: true,
+        createdAt: { type: Date, default: Date.now }
+        },
+     mobile :{
+        type: String,
+        require: true
+    },
+     city: {
+        type: String,
+        require: true
+    },
+     fulladress: {
+        type: String,
+        require: true
+    },
+     pincode: {
+        type: String,
+        require: true
+    },
+     statename: {
+        type: String,
+        require: true
+    },
+}) 
+
+module.exports = mongoose.model("customer", customerSchema);
