12da597b72b4dfa82c7c5a08ef537f608ce4826a
praveen choudhary <pc852180@gmail.com>
2025-03-12 16:04:04 +0530

    E-Banking-Project

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     <div id="header">
     {/* <img src="bank.jpeg" width="150px" height="100px" style={{paddingBottom:"20px"}} alt="" /> */}
     <img src="pic3.jpeg" width="250px" height="170px" style={{paddingBottom:"20px"}} alt="" />
-    <h1 style={{paddingTop:"40px"}}>E-Banking-Project-Site </h1>
+    <h1 style={{paddingTop:"40px"}}>Welcome To E-Banking-Project</h1>
         
     </div>
     </>
